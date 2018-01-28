f9f86109762997428ce77c9a06a4825903679fa7
tochman <thomas@craftacademy.se>
2018-01-28 01:08:17 +0100

    adds unit tests for components using jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {mount} from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('display "Contacts"', () => {
+    const wrapper = mount(<App/>);
+    expect(wrapper.text()).toContain('Contacts')
+})
--- src/components/ContactCard.test.js
+++ src/components/ContactCard.test.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import ContactCard from './ContactCard';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {mount} from 'enzyme'
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    const contact = {
-        attributes: {
-            name: 'Thomas',
-            location: 'Gothenburg',
-            image: 'http://example.com',
 
-        }
-    }
-    ReactDOM.render(<ContactCard contact={contact}/>, div);
-    ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+let wrapper, contact;
+
+describe('<ContactCard />', ()=>{
+    beforeEach(() => {
+        contact = {
+            attributes: {
+                name: 'Thomas',
+                location: 'Gothenburg',
+                image: 'http://example.com',
+                twitter: 'thomasochman',
+                role: 'Developer'
+
+            }
+        };
+        wrapper = mount(<ContactCard contact={contact}/>);
+    });
+
+    it('displays contact name', () => {
+        expect(wrapper.text()).toContain('Thomas');
+    });
+
+    it('displays contact location', () => {
+        expect(wrapper.text()).toContain('Gothenburg')
+    });
+
+    it('displays contact twitter', () => {
+        expect(wrapper.text()).toContain('@thomasochman')
+    });
+
 });
+
